fc9ad1c6c24d551c0da963f2defd075d914a249a
Ritu Thakur <rituthakur7877@gmail.com>
2022-08-22 20:27:48 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// var fullname = "ritu";
+
+// //how to use variables
+
+// // fullname
+// // full-name
+// // full-name
+// // fullname
+
+// var couseName = "react js bootstrape";
+
+// var isloggedia = false;
+
+// var loggedCount = "34";
+
+
+// // console.log(loggedCount);
+// // console.log("loggedcount");
+
+// var paymentMode;
+
+// paymentMode = "create card";
+// console/log(paymentMode);
+
+// var if = 23;
